5a2e0d4c274f4ad568f7cf8fac2470911d08c4e7
xaviguasch <xaviguasch@gmail.com>
2019-05-03 12:02:16 +0200

    binded method with argument

--- src/ButtonList.js
+++ src/ButtonList.js
@@ -6,12 +6,25 @@ class ButtonList extends Component {
     colors: ['#e056fd', '#eb4d4b', '#badc58', '#f0932b']
   }
 
+  constructor(props) {
+    super(props)
+    this.state = { color: 'cyan' }
+  }
+
+  changeColor(newColor) {
+    this.setState({ color: newColor })
+  }
+
   render() {
     return (
-      <div className='ButtonList'>
+      <div className='ButtonList' style={{ background: this.state.color }}>
         {this.props.colors.map(c => {
           const colorObj = { backgroundColor: c }
-          return <button style={colorObj}>Click on me!</button>
+          return (
+            <button style={colorObj} onClick={this.changeColor.bind(this, c)}>
+              Click on me!
+            </button>
+          )
         })}
       </div>
     )
